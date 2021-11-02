5edf5d278d0dff9d58fa7bf55b62c9bfe2c090d4
Mehrzad <USER@DESKTOP-6HPS0N7>
2021-11-02 13:43:05 +0330

    order info controller error handling

--- lib/Controller/Controllers/Order/OrderInfoController.dart
+++ lib/Controller/Controllers/Order/OrderInfoController.dart
@@ -1,18 +1,24 @@
 import 'package:copapp/Controller/Service/OrderService.dart';
 import 'package:copapp/Model/Order/OrderHeader.dart';
+import 'package:copapp/Utilities/Snacki.dart';
 import 'package:get/get.dart';
 
 class OrderInfoController extends GetxController {
   bool isLoading = false;
   OrderHeader? order;
-  
+
   getOrder(orderId) async {
     try {
       isLoading = false;
       update(['loading']);
       await OrderServiceV2().GetOrderInfo(orderId).then((value) {
-        order = value.data;
+        if (value.isSuccess)
+          order = value.data;
+        else
+          value.showMessage();
       });
+    } catch (e) {
+      Snacki().GETSnackBar(false, 'مشکلی در ارتباط با سرور بوجود آمده است');
     } finally {
       isLoading = false;
       update(['loading']);
